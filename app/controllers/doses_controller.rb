d4f922651cca30e5edf987338d259c2bfc74d08b
Christian Doumit <christian.doumit@gmail.com>
2019-11-14 16:18:32 -0500

    Cocktails and Doses controllers initialized

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new()
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    @dose.save
+
+    redirect_to cocktails_path(@cocktail)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:name)
+  end
+end
